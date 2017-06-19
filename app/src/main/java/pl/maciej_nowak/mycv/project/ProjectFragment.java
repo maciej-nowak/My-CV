50102ee4fb4c1573a007454c924cfecd05064b10
madjer22 <maciej.nowak.zslg@gmail.com>
2017-06-19 10:14:05 +0200

    feature_projects - Project fragment added

---
+++ app/src/main/java/pl/maciej_nowak/mycv/project/ProjectFragment.java
@@ -0,0 +1,44 @@
+package pl.maciej_nowak.mycv.project;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pl.maciej_nowak.mycv.R;
+
+/**
+ * Created by Maciej on 19.06.2017.
+ */
+
+public class ProjectFragment extends Fragment {
+
+    public static final String TAG = "AboutFragment";
+    private RecyclerView recyclerView;
+
+    public ProjectFragment() {
+    }
+
+    public static ProjectFragment newInstance() {
+        ProjectFragment fragment = new ProjectFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_project, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.project_container);
+        return view;
+    }
+}
