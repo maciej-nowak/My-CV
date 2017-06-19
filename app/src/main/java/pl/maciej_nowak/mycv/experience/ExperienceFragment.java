a2602138f092f8b75b0689407d6f6dea41fb01f7
madjer22 <maciej.nowak.zslg@gmail.com>
2017-06-19 09:10:00 +0200

    feature_experience - Experience fragment and dependencies added

---
+++ app/src/main/java/pl/maciej_nowak/mycv/experience/ExperienceFragment.java
@@ -0,0 +1,45 @@
+package pl.maciej_nowak.mycv.experience;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pl.maciej_nowak.mycv.R;
+
+/**
+ * Created by Maciej on 19.06.2017.
+ */
+
+public class ExperienceFragment extends Fragment {
+
+    public static final String TAG = "EexperienceFragment";
+
+    private RecyclerView recyclerView;
+
+    public ExperienceFragment() {
+    }
+
+    public static ExperienceFragment newInstance() {
+        ExperienceFragment fragment = new ExperienceFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_experience, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.experience_container);
+        return view;
+    }
+}
